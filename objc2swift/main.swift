0beaffdbd8b9ff1a46e1f14ac00bda20636a8731
Darren Clark <darrenjoelclark@icloud.com>
2015-10-30 19:55:46 -0700

    Initial commit

---
+++ objc2swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  objc2swift
+//
+//  Created by Darren Clark on 2015-10-30.
+//  Copyright © 2015 Darren Clark. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
