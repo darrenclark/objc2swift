0e297d1bee18ef238bda4ba31d111311333363b6
Darren Clark <darrenjoelclark@icloud.com>
2015-10-30 20:31:55 -0700

    Added libclang references/headers

---
+++ objc2swift/objc2swift-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <complex.h>
+#undef I
+
+#import "clang-c/CXCompilationDatabase.h"
+#import "clang-c/Index.h"
+#import "clang-c/Platform.h"
+#import "clang-c/Documentation.h"
+#import "clang-c/CXString.h"
+#import "clang-c/CXErrorCode.h"
+#import "clang-c/BuildSystem.h"
